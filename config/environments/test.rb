c7a4ca5d8f66cbf8897c804b9c3291050079b276
cassiano yasumitsu <cassiano@usuarioincomum.com>
2023-05-01 09:34:32 +0900

    fix: company

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -16,10 +16,10 @@ Rails.application.configure do
   # system, or in some way before deploying your code.
   config.eager_load = ENV["CI"].present?
 
-  # Configure public file server for tests with Cache-Control for performance.
+  # Configure requests file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    "Cache-Control" => "public, max-age=#{1.hour.to_i}"
+    "Cache-Control" => "requests, max-age=#{1.hour.to_i}"
   }
 
   # Show full error reports and disable caching.
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :requests
+
 
   # Defines the root path route ("/")
   # root "articles#index"
