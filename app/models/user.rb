32e9135af1c50dee7eab85f187aa192448cc588f
cassiano yasumitsu <cassiano@usuarioincomum.com>
2023-05-15 20:14:52 +0900

    feat: new show

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :furigana, :nihongo, :gender, :phone, :postal_code, :address, :job, presence: true
 
-  validates :manager, :factory, :role, :status, presence: true, on: :update
+  validates :manager, :factory, :role, :contractor, :status, presence: true, on: :update
 
 end
